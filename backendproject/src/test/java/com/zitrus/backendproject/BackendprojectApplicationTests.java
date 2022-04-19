974fac9883072740eba3c95eefe0dc5775fd5782
leoafj <leoafj@hotmail.com>
2022-04-19 18:27:28 -0300

    Projeto backend Zitrus

---
+++ backendproject/src/test/java/com/zitrus/backendproject/BackendprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zitrus.backendproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
